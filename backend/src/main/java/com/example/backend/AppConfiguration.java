cbce096d18b21a1d07799553a8f57494415ea121
bluetree2 <kmsid77@gmail.com>
2025-07-08 16:40:48 +0900

    글 쓰기 진행중

--- backend/src/main/java/com/example/backend/AppConfiguration.java
+++ backend/src/main/java/com/example/backend/AppConfiguration.java
@@ -3,6 +3,7 @@ package com.example.backend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,9 +11,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class AppConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(httpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> c.disable());
-        
+
         return http.build();
     }
 
---
+++ backend/src/main/java/com/example/backend/BoardController.java
@@ -0,0 +1,20 @@
+package com.example.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/board")
+public class BoardController {
+
+    @RequestMapping("add")
+    @ResponseBody
+    public String add(@RequestBody BoardDto dto) {
+        System.out.println(dto);
+
+        return "board/add";
+    }
+}
