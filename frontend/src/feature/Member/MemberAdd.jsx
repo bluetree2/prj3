99069773790c9d8d5c0621ae306b6b3b19ed6450
bluetree2 <kmsid77@gmail.com>
2025-07-10 09:30:48 +0900

    회원 가입 화면

---
+++ frontend/src/feature/Member/MemberAdd.jsx
@@ -0,0 +1,88 @@
+import { Col, FormControl, FormGroup, FormLabel, Row } from "react-bootstrap";
+import { useState } from "react";
+
+export function MemberAdd() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [nickname, setNickname] = useState();
+  const [info, setInfo] = useState();
+
+  function handleSaveClick() {
+    //post /api/member/add, {email,password,nickname,info}
+    axios
+      .post("/api/member/add", {
+        email: email,
+        password: password,
+        nickname: nickname,
+        info: info,
+      })
+      .then((res) => {
+        console.log("good");
+      })
+      .catch((err) => {
+        console.log("bad");
+      })
+      .finally(() => console.log("always"));
+  }
+
+  return (
+    <Row className="justify-content-center">
+      <Col xs={12} md={8} lg={6}>
+        <h2 className="mb-4">회원 가입</h2>
+        <div>
+          <FormGroup className="mb-3" controlId="email1">
+            <FormLabel>이메일</FormLabel>
+            <FormControl
+              type={"email"}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup controlId="password1">
+            <FormLabel>암호</FormLabel>
+            {/*type는 password인데 보이도록 text로 둠*/}
+            <FormControl
+              type={"password1"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup controlId="password2">
+            {/*todo 나중에 적용*/}
+            <FormLabel>암호 확인</FormLabel>
+            <FormControl
+              type={"password1"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FormControl type={"password2"} />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup>
+            <FormLabel controlId="nickname1">별명</FormLabel>
+            <FormControl
+              type={"password1"}
+              onChange={(e) => setNickname(e.target.value)}
+            />
+            <FormControl type={"nickname1"} />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup>
+            <FormLabel controlId="info1">자기소개</FormLabel>
+            <FormControl
+              type={"password1"}
+              onChange={(e) => setInfo(e.target.value)}
+            />
+            <FormControl as={"textarea"} rows={6} />
+          </FormGroup>
+        </div>
+        <div>
+          <Button onClick={handleSaveClick}>가입</Button>
+        </div>
+      </Col>
+    </Row>
+  );
+}
